43c822b3ce7b629f98e5d8de8cdc9c168a9d6d4a
Antonio Oliveira <antonioeduardo1998@gmail.com>
2021-10-09 20:38:24 -0300

    replace jasmine with jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'all-my-front-end-knowledge'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('all-my-front-end-knowledge');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'all-my-front-end-knowledge app is running!'
-    );
-  });
 });
---
+++ stylelint.config.js
@@ -0,0 +1,63 @@
+const namePattern = /([a-z0-9]+|#\{\$.*\})((-[a-z0-9]+|#\{\$.*\})+)?/;
+
+module.exports = {
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-config-sass-guidelines",
+    "stylelint-prettier/recommended",
+  ],
+  plugins: [
+    "stylelint-color-format",
+    "stylelint-prettier",
+    "stylelint-selector-bem-pattern",
+  ],
+  rules: {
+    "alpha-value-notation": "percentage",
+    "at-rule-disallowed-list": ["import", "media"],
+    "color-format/format": {
+      format: "hsl",
+    },
+    "color-function-notation": "legacy", // TODO: change when https://github.com/microsoft/vscode/issues/43111 is resolved
+    "declaration-empty-line-before": "never",
+    "declaration-no-important": true,
+    "function-parentheses-space-inside": "never-single-line",
+    "hue-degree-notation": "number",
+    "max-nesting-depth": [
+      1,
+      {
+        ignore: "pseudo-classes",
+        ignoreAtRules: ["each", "include", "media", "supports"],
+      },
+    ],
+    "no-empty-source": null,
+    "no-descending-specificity": null,
+    "plugin/selector-bem-pattern": {
+      preset: "bem",
+      implicitComponents: [
+        "src/**/*.component.scss",
+        "src/styles/directives/**/*.scss",
+      ],
+      componentName: "[a-z]+",
+      componentSelectors: (componentName) =>
+        new RegExp(
+          `^\\.(${componentName})(((--|__)(${namePattern.source}))+)?$`
+        ),
+      ignoreSelectors: [/^\.mat-.+$/, /^\.cdk-.+$/, /^%.+/],
+    },
+    "prettier/prettier": [
+      true,
+      {
+        singleQuote: true,
+        printWidth: 100,
+      },
+    ],
+    "rule-empty-line-before": [
+      "always",
+      {
+        ignore: ["first-nested"],
+      },
+    ],
+    "selector-class-pattern": null,
+    "string-quotes": "single",
+  },
+};
